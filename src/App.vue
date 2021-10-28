053676d28adaa814ea3e1f45814c77ab104b076f
salar <salarmail12@gmail.com>
2021-10-28 11:05:48 +0200

    New fix

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Open Sans",serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
