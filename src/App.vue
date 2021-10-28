e405263d58db1c41d4a856d621f946884dac9ad2
salar <salarmail12@gmail.com>
2021-10-28 10:08:19 +0200

    Minor fix

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-
+  <div id="app">
   </div>
 </template>
 
@@ -13,3 +12,13 @@ export default {
 }
 </script>
 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
