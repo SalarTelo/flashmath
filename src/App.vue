68a6163865c94f8da9a08d739f1d578dfccb06ca
penzija <luka.kik@iths.se>
2021-10-28 11:32:21 +0200

    Put background color

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
+
+    <Game></Game>
   </div>
 </template>
 
 <script>
 
+import Game from '@/components/Game.vue';
+
+
 export default {
   name: 'App',
   components: {
+    Game
   }
 }
 </script>
---
+++ src/components/Game.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="background">
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Game",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.background {
+  background-color: red;
+
+}
+</style>
