dd36aba5e6b6461fed274fb672a03e9a13466fa3
penzija <luka.kik@iths.se>
2021-11-11 16:19:31 +0100

    Added third database, answered questions list

--- backend/database.js
+++ backend/database.js
@@ -2,6 +2,7 @@ const sqlite3 = require('sqlite3').verbose()
 
 const DBSOURCE = "flashmath.db"
 const DBSOURCE2 = "subusers.db"
+const DBSOURCE3 = "answeredQuestionsList.db"
 
 
 let questionDb = new sqlite3.Database(DBSOURCE, (err) => {
@@ -9,7 +10,7 @@ let questionDb = new sqlite3.Database(DBSOURCE, (err) => {
         console.error(err.message)
         throw err
     } else {
-        console.log('Connected to the SQlite database.')
+        console.log('Connected to the questionDb database.')
         questionDb.run(`CREATE TABLE flashmath (
             questionId INTEGER PRIMARY KEY,
             question TEXT,
@@ -17,9 +18,9 @@ let questionDb = new sqlite3.Database(DBSOURCE, (err) => {
             category INT
             )`, (err) => {
             if (err) {
-                console.log(err)
+                console.log("Table already present!")
             } else {
-                console.log("Table just created, creating some rows")
+                console.log("questionDB table just created, creating some rows")
                 let insert = 'INSERT INTO flashmath (question, answer) VALUES (?,?)'
                 questionDb.run(insert, ["2+2", 4])
             }
@@ -34,12 +35,12 @@ let subuserDb = new sqlite3.Database(DBSOURCE2, (err) => {
     } else {
         console.log('Connected to the SQlite database.')
         subuserDb.run(`CREATE TABLE subusers (
-            userID INTEGER PRIMARY KEY,
+            userId INTEGER PRIMARY KEY,
             Name TEXT,
             answered INT
             )`, (err) => {
             if (err) {
-                console.log(err)
+                console.log("Table already present!")
             } else {
                 console.log("Table just created, creating some rows")
                 let insert = 'INSERT INTO subusers (Name, answered) VALUES (?,?)'
@@ -49,6 +50,25 @@ let subuserDb = new sqlite3.Database(DBSOURCE2, (err) => {
     }
 })
 
+let answeredQuestionsList = new sqlite3.Database(DBSOURCE3, (err) => {
+    if (err) {
+        console.error(err.message)
+        throw err
+    } else {
+        console.log('Connected to the SQlite database.')
+        answeredQuestionsList.run(`CREATE TABLE answeredquestionslist (
+id INTEGER PRIMARY KEY, 
+userID INT REFERENCES subusers (userID),
+questionID INT REFERENCES flashmath(questionID)
+)`, (err) => {
+            if (err) {
+                console.log("Table already present!")
+            } else {
+                console.log("Table just created")
+            }
+        })
+    }
+})
 
-module.exports = {questionDb, subuserDb}
 
+module.exports = {questionDb, subuserDb, answeredQuestionsList}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-const {questionDb, subuserDb} = require("./database.js");
+const {questionDb, subuserDb, answeredQuestionsList} = require("./database.js");
 
 app.use(cors());
 app.use(express.static('public'));
@@ -16,6 +16,8 @@ const HTTP_PORT = 3000
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
 });
+
+// GET ALL REQUESTS
 
 app.get("/content", (req, res, next) => {
     let sql = "select * from flashmath"
@@ -47,21 +49,70 @@ app.get("/users", (req, res, next) => {
     });
 });
 
-
-app.get("/content/:id", (req, res, next) => {
-    let sql = "select * from flashmath where questionId = ?"
-    let params = [req.params.id]
-    questionDB.get(sql, params, (err, row) => {
+app.get("/answeredlist", (req, res, next) => {
+    let sql = "select * from answeredquestionslist"
+    let params = []
+    answeredQuestionsList.all(sql, params, (err, rows) => {
         if (err) {
             res.status(400).json({"error": err.message});
             return;
         }
         res.json({
-            // "message": "success",
+            "message": "success",
+            "answered questions list": rows
+        })
+    });
+});
+
+
+// GET BY ID REQUESTS
+
+app.get("/content/:id", (req, res, next) => {
+    let sql = "select * from flashmath where questionId = ?"
+    let params = [req.params.id]
+    questionDb.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({"error": err.message});
+            return;
+        }
+        res.json({
+            "message": "success",
             "flashmath": row
         })
     });
 });
+
+app.get("/users/:id", (req, res, next) => {
+    let sql = "select * from subusers where userId = ?"
+    let params = [req.params.id]
+    subuserDb.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({"error": err.message});
+            return;
+        }
+        res.json({
+            "message": "success",
+            "flashmath": row
+        })
+    });
+});
+
+app.get("/answeredlist/:id", (req, res, next) => {
+    let sql = "select * from answeredquestionslist where Id = ?"
+    let params = [req.params.id]
+    answeredQuestionsList.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({"error": err.message});
+            return;
+        }
+        res.json({
+            "message": "success",
+            "flashmath": row
+        })
+    });
+});
+
+//POST requests
 
 app.post("/content/", (req, res, next) => {
     let data = {
